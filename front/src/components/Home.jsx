5fb338aefc1bbdbad1a2d1169f518222815ad957
Luisa Gil <luisamariagilgarcia24@gmail.com>
2021-07-16 16:04:00 -0500

    Se termina el home

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -6,10 +6,9 @@ const Home = () =>{
             <div className="row">
                 <div className="col-md-3"/>
                 <div className="col-md-6">
-                    <br/><br/>
-                    <h1 className="cover-heading mt-5 font-weight-bold">Welcome to Serie App <i
-                        className="bi bi-controller" /></h1>
-                        <img src={"https://static1.colliderimages.com/wordpress/wp-content/uploads/2020/11/Best-tv-shows-Netflix-V3.jpg"} />
+                    <h1 className="cover-heading mt-5 font-weight-bold"> Welcome to Serie App </h1>
+                    <br></br>
+                        <img className="col-md-12" src={"https://static1.colliderimages.com/wordpress/wp-content/uploads/2020/11/Best-tv-shows-Netflix-V3.jpg" } />
                     <h4 className={"color-black mt-5"}>Here you can add and see a list of recommended series</h4>
                     <br/><br/>
                 </div>
