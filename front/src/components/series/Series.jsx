bdcf7c7bd8eb0c4350914f4900ad0632eba63225
Luisa Gil <luisamariagilgarcia24@gmail.com>
2021-07-15 23:28:26 -0500

    Se realizan cambios front

--- front/src/components/series/Series.jsx
+++ front/src/components/series/Series.jsx
@@ -22,9 +22,22 @@ const Series = () => {
                                 <div className="card-body">
                                     <div key={element.id}>
                                         <h4 className="card-title"> {element.nombre} </h4>
-                                        <h5> {element.categoriaId}</h5>
+                                        <h5> 
+                                            {(element.categoriaId== "60f083d8eb32e255d5c99a50")?Terror:null}
+                                            {(element.categoriaId== "60f083ceeb32e255d5c99a4f")?Action:null}
+                                            {(element.categoriaId=='60f0a640bded8d6c0801de0e')?Comedy:null}
+                                            {(element.categoriaId=='60f083f1eb32e255d5c99a52')?Anime:null}
+                                            {(element.categoriaId=='60f083afeb32e255d5c99a4e')?Sciencefiction:null}
+                                        </h5>
                                         <p> {element.numeroTemporadas}  </p>
                                         <img src={element.urlImagen} className={"card-img"}/>
+                                        <button type="submit" className="btn btn-primary " style="width:40px; height:40px; float:right">
+                                        <i class="bi bi-pencil-square"></i>
+                                        </button>
+                                        <button type="submit" className="btn btn-danger" style="width:40px; height:40px; float:right">
+                                        <i class="bi bi-trash"></i>
+                                        </button>
+
                                     </div>
                                 </div>
                             </div>
